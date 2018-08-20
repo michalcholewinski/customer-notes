ed212f14e028bc504b218a51a432bd45c13f4bba
Michał Cholewiński <cholewinskimichal@gmail.com>
2018-08-20 23:19:42 +0200

    Initial commit

---
+++ src/main/java/com/customernotes/CustomerNotes/CustomerNotesApplication.java
@@ -0,0 +1,12 @@
+package com.customernotes.CustomerNotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerNotesApplication.class, args);
+	}
+}
